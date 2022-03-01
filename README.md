00628c6f1832a1deddacacf1ea1b43295d36b18e
valdecar <valdecar@ya.ru>
2022-03-01 13:57:55 +0300

    added separated sampling code from tokentranslator play around "pip install --install-option" with subprocess fixed some tests

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # proposalsampler
-coming soon
+Experimental project. For given proposal or equation it generate its implementation (context or sample space $\Omega$ or model (like model for theory in logic therms))
+at which this proposal (or equation) is true.
+For example below, given the sent "abelian(G) \\and subgroup(H, G,) => abelian(H)", the program will generate some groups (described with they generators (like in GAP)) for G, H, at which this proposal is true (i.e. $\Omega = {\{H_{i}, G_{i}\} | where i \leq n}$ where $n$ is count of samples to be generated).
+Only part of that "arguments" of the proposal can be given ($\Theta$) such that only remained part (\Psi) ought to be generated (so $\Omega = \Theta + \Psi$).  
+The main idea is that such samples can give you beter understanding of proposal itself, especially if such proposal have been taken from some abstract math theory. 
+
+##### sampling proposals:
+
+```
+from tokentranslator.db_models.model_main import TokenizerDB
+from tokentranslator.env.clause.clause_main import Clause
+
+model = TokenizerDB()
+
+# switch to clauses db:
+model.change_dialect_db("cs")
+
+clause = Clause("abelian(G) \\and subgroup(H, G,) => abelian(H)", db=model)
+clause.parser.parse()
+
+# now import and use sampling:
+from proposalsampler.sampling.slambda.tests_slambda_main import test_ventries 
+from proposalsampler.sampling.slambda import slambda_main as sm
+
+# for this example test_ventries[3] is init data for proposal clause:
+test_ventries[3] =
+{'G': ('(1,4)(2,3)', '(1,3)(2,4)'),
+ "['s', 1, 0, 0]": True,
+ "['s', 1, 1, 0]": True,
+ 'idd': "['s']",
+ 'successors_count': 0}
+
+# so group G is given but group H is not, and must be found
+# during sampling:
+sampler = sm.Sampler(clause.net_out, test_ventries[3])
+
+sampler.run()
+# if successors found, they entries would look like:
+
+{'G': ('(1,4)(2,3)', '(1,3)(2,4)'),  'H': ('(1,3)(2,4)', '(1,4)(2,3)')", ['s', 1, 1, 0]": True, "['s', 1, 0, 0]": True, 'idd': "['s', 4, 3, 0]", 'successors_count': 0, 'checked_nodes': ["['s']", "['s', 1]", "['s', 1, 1, 0]", "['s', 1, 0, 0]", "['s', 0, 0]"], 'failure_statuses': {}, 'parent_idd': "['s', 4, 3]", "['s']": True, "['s', 0, 0]": True, "['s', 1]": True}
+
+# so group H found and in this case H = Group('(1,3)(2,4)', '(1,4)(2,3)') (= G but proposal is still holding) 
+# if no results were generated, try run again.
+```
+
+##### sampling equations:
+```
+>>> e = Equation("f(a*x+b*y)=a*f(x)+b*f(y)")
+>>> e.parser.parse()
+>>> e.sampling.sympy.sampling_vars()
+>>> # or e.sampling.sympy.sampling_subs()
+
+>>> e.sampling.sympy.show_rand()
+>>> # or "".join(e.eq_tree.flatten('rand_sympy'))
+
+sin(0.243*0.570+0.369*0.078)=0.243*sin(0.570)+0.369*sin(0.078)
+```
+### Requirements
+```
+linux, python3, tokentranslator
+
+### Installation and running
+```
+pip install proposalsampler -v
+# -v for extended output during installation because
+# some tricks with `subprocess.popen` and `pip` should be watched
+# (see setup.py for more)
+
+### Tests:
+see `tests/test_list.txt`
+
+### References:
+##### Sampling:
+Probabilistic Models of Cognition: https://probmods.org/
+
+(eng: George Pólya: Mathematics and Plausible Reasoning, Princeton University Press 1954, 2 volumes (Vol. 1: Induction and Analogy in Mathematics, Vol. 2: Patterns of Plausible Inference)
+(ru: Джордж Пойа: Математика и правдоподобные рассуждения)
---
+++ setup.py
@@ -0,0 +1,95 @@
+import subprocess
+from setuptools import setup, find_packages
+
+from setuptools_scm import get_version
+# from setuptools_scm.integration import find_files
+
+
+if __name__ == "__main__":
+    print("\nDEV: find_packages:")
+    lpackages = find_packages('.')
+    print(lpackages)
+    
+    print("\nINFO: get_version:")
+    try:
+        # local_scheme for pypi:
+        print(get_version(root='.', relative_to=__file__,
+                          local_scheme="no-local-version"))
+    except:
+        
+        raise(BaseException("setuptools_scm get_version bug,"
+                            + " update to v 3.5.0 needed:"
+                            + " pip uninstall setuptools_scm"
+                            + "\n pip install -Iv setuptools_scm>=3.5.0"))
+        
+    # for description:
+    with open("README.md") as f:
+        long_description = f.read()
+    
+    # for requirements:
+    with open("requirements.txt") as f:
+        s_reguirements = f.read()
+    requirements = s_reguirements.split("\n")
+    print("\nINFO: requirements:")
+    print(requirements)
+
+    # currently impossible to install project dependency with
+    # `--install-option` argument with setuptools `install_requires`
+    # this is only way to install dependency with additional
+    # arguments automatically:
+    # (TODO: maybe just use `pip install -r requirements.txt`?
+    #  (pip support `--install-option` in requirements.txt))
+    cmd = ['pip', 'install', '--install-option=--dialect=tex', 'tokentranslator']
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE) as proc:
+        out = proc.stdout.read().decode().split("\n")
+    print("\n DEV: FOR pip install tokentranslator")
+    for line in out:
+        print(line)
+    print("\n DEV: END FOR pip install tokentranslator")
+
+    installed = setup(
+        
+        name="proposalsampler",
+
+        # use version (only) from setuptools_scm:
+        use_scm_version={
+            "root": ".",
+            "relative_to": __file__,
+            "local_scheme": "no-local-version"
+        },
+
+        # use_scm_version=True,
+        # version=get_version(root='.', relative_to=__file__),
+
+        author="tokentranslator-group",
+        author_email="",
+        description="proposal sampler",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/tokentranslator-group/proposalsampler",
+        packages=find_packages('.'),
+        include_package_data=True,
+        
+        # exclude_package_data={"hybriddomain.tests":
+        #                       ["*.json", "*.ipynb",
+        #                        "problems/*", "settings/*"]},
+        # for ``include_package_data`` to work accordingly to git:
+        # setuptools_scm will register itself as setuptools plug in,
+        # with use of ``entry_points``, so it's git/hg ``file_finders``
+        # will be implicitly used for finding only git/hg managable files
+        # (that not in .{git/hg}ignore) when ``include_package_data``
+        # or ``package_data`` attributes of setuptools.setup() was used.
+        # (see https://github.com/pypa/setuptools_scm/blob/master/setup.py)
+
+        # this will automatically used due to setup_requires setuptools_scm
+        # (it will add it's entry_points to the setuptools during install):
+        # entry_points="""
+        # [setuptools.file_finders]
+        # setuptools_scm = setuptools_scm.integration:find_files
+        # """,
+
+        setup_requires=['setuptools_scm >= 3.5.0'],
+        # setup_requires=[ "setuptools_git >= 0.3", ],
+        install_requires=requirements
+    )
+    
